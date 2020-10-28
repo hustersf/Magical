50173d25c79ecb76215629659156d75c848c4f76
sufan <sofarsogood001@163.com>
2020-10-28 19:32:45 +0800

    项目创建

---
+++ packages/apollo/src/main/java/com/sofar/apollo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sofar.apollo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sofar.apollo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
