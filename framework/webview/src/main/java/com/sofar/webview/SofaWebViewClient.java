17902c315df261a407392dfc5ca538bf0e375fda
sufan <sofarsogood001@163.com>
2022-10-17 11:00:42 +0800

    玩安卓-webview 拦截不认识的url

--- framework/webview/src/main/java/com/sofar/webview/SofaWebViewClient.java
+++ framework/webview/src/main/java/com/sofar/webview/SofaWebViewClient.java
@@ -2,10 +2,14 @@ package com.sofar.webview;
 
 import android.graphics.Bitmap;
 import android.util.Log;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class SofaWebViewClient extends WebViewClient {
+
+  public static final String HTTP_SCHEME = "http://";
+  public static final String HTTPS_SCHEME = "https://";
 
   @Override
   public void onPageStarted(WebView view, String url, Bitmap favicon) {
@@ -17,5 +21,16 @@ public class SofaWebViewClient extends WebViewClient {
   public void onPageFinished(WebView view, String url) {
     super.onPageFinished(view, url);
     Log.d(Const.TAG, "onPageFinished:" + url);
+  }
+
+  @Override
+  public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+    String url = request.getUrl().toString();
+    Log.d(Const.TAG, "shouldOverrideUrlLoading:" + url);
+    if (url.startsWith(HTTP_SCHEME) || url.startsWith(HTTPS_SCHEME)) {
+      return false;
+    }
+    //拦截不认识的url
+    return true;
   }
 }
